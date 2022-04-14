cdfe072a1cd554af8a0e741b3e5b80bb3457b1b8
GLima727 <guilhermegutierrez727@gmail.com>
2022-04-14 13:51:36 +0100

    buttons

--- MainMenu/code.js
+++ MainMenu/code.js
@@ -1,22 +1,29 @@
 
 function openInstructions() {
     var container = document.getElementById('Instructions');
-    container.style.display = 'grid';
+    container.style.animation = "fadeIn 0.5s linear, float 3s ease-in-out infinite"
+    container.style.display = 'block';
 }
 function openSettings() {
     var container = document.getElementById('Settings');
+    container.style.animation = "fadeIn 0.5s linear, float 3s ease-in-out infinite"
     container.style.display = 'grid';
+}
+
+function closeSettings() {
+    var container = document.getElementById('Settings');
+    document.getElementById("slowSpeedButton").style.backgroundColor = "#d6eaec"; 
+    document.getElementById("normalSpeedButton").style.backgroundColor = "#d6eaec"; 
+    document.getElementById("fastSpeedButton").style.backgroundColor = "#d6eaec"; 
+    container.style.display = 'none';
 }
 
 document.addEventListener('mouseup', function(e) {
     var container = document.getElementById('Instructions');
+
     if (!container.contains(e.target)) {
         container.style.display = 'none';
     }
+
 });
-document.addEventListener('mouseup', function(e) {
-    var container2 = document.getElementById('Settings');
-    if (!container2.contains(e.target)) {
-        container2.style.display = 'none';
-    }
-});
+
